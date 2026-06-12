8b4a76be740b26c3f7701d7ce63183bf6a28ee07
resolushh <resolushhh@gmail.com>
2026-06-12 04:41:12 +0300

    feat(subjects): добавить модуль дисциплин

---
+++ subjects.py
@@ -0,0 +1,11 @@
+SUBJECTS = [
+    "Реляционные базы данных",
+    "Программирование на Python",
+    "Разработка программного обеспечения",
+]
+
+
+def print_subjects():
+    print("Дисциплины:")
+    for subject in SUBJECTS:
+        print(f"  - {subject}")
