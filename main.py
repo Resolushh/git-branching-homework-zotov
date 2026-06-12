0aaadfd40c857a776df6fab6d96fe1f9272be8bd
resolushh <resolushhh@gmail.com>
2026-06-12 04:41:22 +0300

    feat(report): подключить отчёт в main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from profile import print_profile
+from report import print_report
 from subjects import print_subjects
 
 print("Student Branching App")
 print("Проект для тренировки веток Git")
 print_profile()
 print_subjects()
+print_report()
